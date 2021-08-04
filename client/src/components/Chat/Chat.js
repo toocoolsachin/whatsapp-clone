8ece65ad8b5675568cae7a813bc113df8ff2a4f4
Sachin Kumar <sachin.k@safe.security>
2021-08-04 13:08:30 +0530

    fix api

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -9,7 +9,7 @@ import Input from "../Input/Input";
 
 import "./Chat.css";
 
-const ENDPOINT = "http://localhost:5000/";
+const ENDPOINT = "https://whatsapp-clone-backend-node.herokuapp.com/";
 
 let socket;
 
